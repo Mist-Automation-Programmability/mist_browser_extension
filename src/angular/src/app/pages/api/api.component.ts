78ea86f0298f9d86521319538febe0e817b2c762
Thomas Munzer <thomas.munzer@gmail.com>
2021-07-16 09:24:36 +0200

    fix (rf)templates quick links

--- src/angular/src/app/pages/api/api.component.ts
+++ src/angular/src/app/pages/api/api.component.ts
@@ -464,7 +464,7 @@ export class ApiComponent implements OnInit {
           this.forgeSiteSwitchConfig(res.groups.host);
           break;
         case "pcap":
-          this.setName(res.groups.obj.substr(0, res.groups.obj.length - 1), res.groups.detail);
+          this.setName(res.groups.obj, res.groups.detail);
           this.forgeSiteObject("pcaps", res.groups.host, res.groups.detail);
           break;
         // ORG
@@ -721,13 +721,15 @@ export class ApiComponent implements OnInit {
     const sle_re = /https:\/\/manage\.(?<host>[a-z0-1.]*mist\.com)\/admin\/\?org_id=(?<org_id>[0-9a-f-]*)#!dashboard\/(?<detail>serviceLevels|wiredserviceLevels|wanserviceLevels)\/(?<scope>[a-z-]*)\/(?<scope_id>[a-f0-9-]*)\/(?<period>[0-9a-z-]*)\/(?<start>[0-9]*)\/(?<stop>[0-9]*)\/(?<site_id>[a-f0-9-]*)/iys;
     const insights_re = /https:\/\/(manage|integration)\.(?<host>[a-z0-1.]*mist\.com)\/admin\/\?org_id=(?<org_id>[0-9a-f-]*)#!dashboard\/insights\/((?<obj>[a-z]+)\/)?((?<obj_id>[a-z0-9-]+)\/)((?<period>[a-z0-9]+)\/)?((?<start>[0-9]*)\/)?((?<stop>[0-9]*)\/)?(?<site_id>[0-9a-f-]*)?/iys;
     const alarm_re = /https:\/\/manage\.(?<host>[a-z0-1.]*mist\.com)\/admin\/\?org_id=(?<org_id>[0-9a-f-]*)#!alerts\/?(?<scope>org|site)?\/?(?<uuid>[0-9a-z-]*)\/?(?<period>[0-9a-z]*)?\/?(?<start>[0-9]*)?\/?(?<stop>[0-9]*)?\/?(?<show_ack>true|false)?\/?(?<group>[a-z%0-9]*)?\/?(?<show_crit>true|false)?\/?(?<show_warn>true|false)?\/?(?<show_info>true|false)?\/?(?<site_id>[0-9a-z-]*)?/iys;
-    const common_re = /https:\/\/(manage|integration)\.(?<host>[a-z0-1.]*mist\.com)\/admin\/\?org_id=(?<org_id>[0-9a-f-]*)#!(?<obj>[a-z]+)\/?((?<detail>detail|template|site|rfTemplate|admin|edgedetail|clusterdetail|new|view)\/)?([0-9]\/)?((?<obj_id>[0-9a-z_-]*)\/)?(?<site_id>[0-9a-f-]*)?/yis;
+    const templates_re = /https:\/\/(manage|integration)\.(?<host>[a-z0-1.]*mist\.com)\/admin\/\?org_id=(?<org_id>[0-9a-f-]*)#!(?<obj>[a-z]+)\/(?<detail>template|rfTemplate)\/(?<obj_id>[0-9a-z_-]*)/yis;
+    const common_re = /https:\/\/(manage|integration)\.(?<host>[a-z0-1.]*mist\.com)\/admin\/\?org_id=(?<org_id>[0-9a-f-]*)#!(?<obj>[a-z]+)\/?((?<detail>detail|site|admin|edgedetail|clusterdetail|new|view)\/)?([0-9]\/)?((?<obj_id>[0-9a-z_-]*)\/)?(?<site_id>[0-9a-f-]*)?/yis;
     const common_objs = ["ap", "gateway", "switch", "assets", "wlan", "tags", "psk", "tunnels", "clients", "sdkclients", "wiredclients", "wxlan", "security", "switchconfig", "pcap", "orgtags", "misttunnels", "switchtemplate", "deviceprofiles", "org", "configuration", "rftemplates", "templates", "auditlogs", "apinventory", "adminconfig", "subscription", "edge"]
 
     const orgsle = orgsle_re.exec(this.tabUrl);
     const sle = sle_re.exec(this.tabUrl);
     const insights = insights_re.exec(this.tabUrl);
     const alarm = alarm_re.exec(this.tabUrl);
+    const templates = templates_re.exec(this.tabUrl);
     const common = common_re.exec(this.tabUrl);
     if (orgsle) {
       this.orgSleUrl(orgsle);
@@ -737,6 +739,8 @@ export class ApiComponent implements OnInit {
       this.insightsUrl(insights);
     } else if (alarm) {
       this.alarmUrl(alarm);
+    } else if (templates) {
+      this.commonUrl(templates);
     } else if (common && common["groups"] && common_objs.includes(common["groups"]["obj"].toLowerCase())) {
       this.commonUrl(common);
     }
