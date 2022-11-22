49d0d11b8a11fbd0901c7f541a9ea2e3d8c223ad
Thomas Munzer <thomas.munzer@gmail.com>
2022-11-22 21:26:34 +0100

    updating scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-./gen_api_struct.sh
-cd ./angular/src
+if [ "$1" ]
+then
+    jq ".version = \"$1\"" ./angular/package.json > ./angular/package.json
+    jq ".version = \"$1\"" ./angular/src/manifest.json > ./angular/src/manifest.json
+fi
+
+cd ./angular
 npm run build:prod
-npx web-ext build -s ../dist -o
+npx web-ext build -s ./dist -o
 cd ../..
---
+++ pack.sh
@@ -0,0 +1,21 @@
+echo "##########################################"
+echo "Building Extension"
+cd ./angular
+npm run build:prod
+npx web-ext build -s ./dist -o
+cd ./web-ext-artifacts/
+echo "##########################################"
+echo "Unpacking ZIP"
+rm -rf mist_extension
+FILE=`ls | grep .zip | sort | tail -n1`
+unzip $FILE -d mist_extension 
+echo "##########################################"
+echo "Chrome packing..."
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=./mist_extension --pack-extension-key=mist_extension.pem
+echo "##########################################"
+echo "Updating Firefox source"
+cd mist_browser_extension
+git pull
+cd ..
+rm mist_browser_extension.tar.gz
+tar czvf mist_browser_extension.tar.gz mist_browser_extension
