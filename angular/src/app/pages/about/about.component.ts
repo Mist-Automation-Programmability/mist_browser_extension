17c4871e021d30ad1f99248a010ff24aaf60e2a3
Thomas Munzer <tmunzer@juniper.net>
2025-03-30 19:55:26 +0200

    update the about section

--- angular/src/app/pages/about/about.component.ts
+++ angular/src/app/pages/about/about.component.ts
@@ -10,7 +10,10 @@ export interface linkElement {
 @Component({
   selector: 'app-about',
   templateUrl: 'about.component.html',
-  styleUrls: ['about.component.scss', '../../scss/button.component.scss']
+  styleUrls: [
+    'about.component.scss',
+    '../../scss/button.component.scss'
+    ]
 })
 export class AboutComponent implements OnInit {
 
