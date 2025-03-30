adc42a6edaf3c0fb9d44879927a7af1bdc40e3e0
Thomas Munzer <tmunzer@juniper.net>
2025-03-31 00:23:42 +0200

    add token usage status message

--- angular/src/app/pages/tools/token_usage/usage.component.ts
+++ angular/src/app/pages/tools/token_usage/usage.component.ts
@@ -42,38 +42,59 @@ export class TokenUsageComponent implements OnInit {
     request_limit: -1,
     request_percentage: 0
   };
+  working:boolean = false;
+  invalid: boolean = false;
+  check_index:number=0;
+  check_total:number=0;
   api_token: string = "";
 
   ngOnInit() {
+    this.invalid = false;
+    this.check_index = 0;
+    this.api_token = "";
+    this._cd.detectChanges()
   }
-
+  
   check_usage() {
     const api_hosts = this._browser.getHostApi()
+    this.working = true;
+    this.invalid = false;
+    this.check_index = 0;
+    this.check_total = api_hosts.length;
     this.check_cloud_usage(api_hosts);
   }
 
-  private check_cloud_usage(api_hosts, index = 0): void {
-    if (index < api_hosts.length) {
+  private check_cloud_usage(api_hosts): void {
+    this._cd.detectChanges()
+    if (this.check_index < api_hosts.length) {
       this._http.get(
-        "https://" + api_hosts[index] + "/api/v1/self/usage",
+        "https://" + api_hosts[this.check_index] + "/api/v1/self/usage",
         { headers: { "Authorization": "Token " + this.api_token }, observe: 'response' }
       ).subscribe(data => {
         if (data.status == 200) {
           this.usage.requests = data.body["requests"];
           this.usage.request_limit = data.body["request_limit"];
           this.usage.request_percentage = (this.usage.requests / this.usage.request_limit) * 100;
+          this.working =false;
           this._cd.detectChanges()
         } else if (data.status == 429) {
           this.usage.requests = 5000;
           this.usage.request_limit = 5000;
           this.usage.request_percentage = 100;
+          this.working =false;
           this._cd.detectChanges()
         } else {
-          this.check_cloud_usage(api_hosts, index + 1);
+          this.check_index +=1;
+          this.check_cloud_usage(api_hosts);
         }
       }, err => {
-        this.check_cloud_usage(api_hosts, index + 1);
+        this.check_index +=1;
+        this.check_cloud_usage(api_hosts);
       })
+    } else {
+      this.working =false;
+      this.invalid = true;
+      this._cd.detectChanges()
     }
   }
 
